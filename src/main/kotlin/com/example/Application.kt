a0b9f4fb54d65797bdf84c45c3f089001c099f97
andreyaksenov <andrey.aksenov@jetbrains.com>
2021-04-14 15:25:07 +0300

    Use EngineMain

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -3,15 +3,13 @@ package com.example
 import io.ktor.application.*
 import io.ktor.response.*
 import io.ktor.routing.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
 
-fun main() {
-    embeddedServer(Netty, port = 8000) {
-        routing {
-            get("/") {
-                call.respondText("Hello, world!")
-            }
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+fun Application.module(testing: Boolean = false) {
+    routing {
+        get("/") {
+            call.respondText("Hello, world!")
         }
-    }.start(wait = true)
+    }
 }
